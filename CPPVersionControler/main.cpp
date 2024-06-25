624704c8c88df28475678039b2da09ccc61369c8
Lukasz Jastrzebski <luka@lukajast.com>
2024-06-25 22:53:10 +0200

    Changes made to RazDwa and GoodBye function on dev,

--- CPPVersionControler/main.cpp
+++ CPPVersionControler/main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
-int main()
-{
-	//Print some text
-	std::cout << "Good day!!!\n\n\n";
-}
 void GoodBye()
 {
 	std::cout << "Good bye!!!\n\n\n";
 }
+void RazDwa()
+{
+	std::cout << "Raz Dwa!!!\n\n\n";
+}
 int main()
 {
-	//greet();
+	RazDwa();
 	GoodBye();
 }
