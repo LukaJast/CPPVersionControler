8d582893c05f273d3f4f7bdbb440eea93fc92484
Lukasz Jastrzebski <luka@lukajast.com>
2024-06-25 22:54:50 +0200

    Changing mian.cpp structure in mainline.

--- CPPVersionControler/main.cpp
+++ CPPVersionControler/main.cpp
@@ -1,15 +1,9 @@
 #include <iostream>
-int main()
-{
-	//Print some text
-	std::cout << "Good day!!!\n\n\n";
-}
+
 void GoodBye()
 {
 	std::cout << "Good bye!!!\n\n\n";
 }
 int main()
 {
-	//greet();
-	GoodBye();
-}
+	GoodBye();}
